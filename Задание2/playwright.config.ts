f961d74eda2bf66bf74e34d3d8cded3eae31ffef
OlgaDubrovina5 <olliegrimm05@gmail.com>
2025-10-01 21:39:58 +0300

    Add files via upload

---
+++ Задание2/playwright.config.ts
@@ -0,0 +1,36 @@
+// playwright.config.js
+// @ts-check
+const { devices } = require('@playwright/test');
+
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+};
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  timeout: 600_000,
+});
+
+module.exports = config;
+
+
+
